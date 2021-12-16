ee5e3b9643c0f6b086679c80d517742ff3073692
Viacheslav Aleksandrov <rimondtt@gmail.com>
2021-12-16 22:39:55 +0200

    second-task-done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients')
+
+const items =  ingredients.map(ingredient => {
+  const item = document.createElement('li');
+  item.classList.add('item')
+  item.textContent = ingredient;
+
+  return item
+})
+
+list.append(...items)
