e572814ad1b4f615ecceaac0137fb0db465aaef9
Viacheslav Aleksandrov <rimondtt@gmail.com>
2021-12-22 23:50:34 +0200

    task08-done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(ev) {
+    ev.preventDefault();
+    const {
+        elements: { email, password }
+    } = ev.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+        return alert('Все поля должны быть заполнены!!!')
+    }
+
+    console.log({
+        email: email.value,
+        password: password.value
+    })
+
+    formRef.reset()
+}
