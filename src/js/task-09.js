d2f2a1742ab8163f2cd0e085c2b9d9cb4de1e98d
Viacheslav Aleksandrov <rimondtt@gmail.com>
2021-12-22 23:58:30 +0200

    task09-done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const btnRef = document.querySelector('.change-color');
+const colorText = document.querySelector('.color');
+
+btnRef.addEventListener('click', onBtnClick);
+
+function onBtnClick() {
+  body.style.backgroundColor = getRandomHexColor();
+  colorText.textContent = body.style.backgroundColor;
+}
